2ef76c097eb837a258d55e2b66918f15f6c8fcbf
Ron Crans <ron@rnp-web.net>
2014-02-16 06:11:41 -0800

    2.9.5a update

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ $cooknam = 'fil_vew' . ($rmtuser ? "_$rmtuser" : '');
 $cookie = $_COOKIE[$cooknam];
 if (!$cookie) { exit; }
 $baseDir = convert_uudecode($cookie).'/';
-$fmxVersion = '2.9.5 - 12 Feb 2014';
+$fmxVersion = '2.9.5 - 16 Feb 2014';
 
 function FileMimeType ($fpath) {
 	$mtyp = 'text/plain';
