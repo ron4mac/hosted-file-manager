85f967ec21f757ff5fde4ab742fe512ae5c30009
Ron Crans <ron@rjconline.net>
2023-09-12 16:04:09 +0000

    v3.5.4

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if ($fmxInJoomla) {
 $cookie = isset($_COOKIE[$cooknam]) ? $_COOKIE[$cooknam] : null;
 if (!$cookie) { exit('Unauthorized'); }
 $baseDir = convert_uudecode($cookie).'/';
-$fmxVersion = '3.5.4 - May 2023';
+$fmxVersion = '3.5.4 - Septemner 2023';
 
 function FileMimeType ($fpath)
 {
